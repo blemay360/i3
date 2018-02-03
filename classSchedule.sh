60d1075ba9849d1c4155f27ae71157fd934f141b
Brandon LeMay <blemay360@gmail.com>
2018-02-02 23:45:08 -0500

    Class schedule program mostly working, but needs restructuring and induction into i3bar

--- classSchedule.sh
+++ classSchedule.sh
@@ -1,16 +1,58 @@
 #!/bin/bash
 filename=classSchedule.txt
 date=$(date)
-date=${date::3}
 while read -r line
 do
 	line="$line"
-	if [ $date = ${line::3} ]; then
+	if [ ${date::3} = ${line::3} ]; then
 		rough=$line
 	fi
 done < "$filename"
 rough=${rough:4}
-echo $rough
-for i in {$#rough}; do
-	echo $i
-done
+tmp=${rough##.}
+firstClass=${rough%.*}
+secondClass=${rough#*.}
+IFS=, read -r -a firstClass <<< "$firstClass"
+IFS=, read -r -a secondClass <<< "$secondClass"
+
+tmp=${date%:??' EST'*}
+len=${#tmp}
+time=${tmp:len-6}
+hours=${time:1:2}
+minutes=${time:4}
+time=$[hours*60+minutes]
+
+tmp=$((${firstClass[0]}))
+firstClassHour=${tmp::2}
+firstClassMinute=${tmp:2}
+firstClassTime=$[firstClassHour*60+firstClassMinute]
+
+tmp=$((${secondClass[0]}))
+secondClassHour=${tmp::2}
+secondClassMinute=${tmp:2}
+secondClassTime=$[secondClassHour*60+secondClassMinute]
+
+function nextClass {
+	if [ $time -lt $firstClassTime ]; then
+		nextClass=${firstClass[1]}
+		nextTime=${firstClassTime}
+	elif [ $time -gt $firstClassTime ] && [ $time -lt $secondClassTime ]; then
+		nextClass=${secondClass[1]}
+		nextTime=${secondClassTime}
+	else
+		nextClass="error in nextClass"
+		nextTime="error in nextClass"
+	fi
+}
+
+function difference {
+	difference=$[$nextTime-$time]
+	remainingMin=$[difference % 60]
+	if [ ${#remainingMin} = 1 ]; then
+		remainingMin=0$remainingMin
+	fi
+	remainingHour=$[difference / 60]
+	echo $remainingHour':'$remainingMin
+}
+nextClass
+difference
